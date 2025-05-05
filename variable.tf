bd09a5fbd535c21f6d66bd6d8c286b52c97d8af2
adworjan <adworjan@redhat.com>
2025-05-05 15:45:30 -0400

    provided default

--- variable.tf
+++ variable.tf
@@ -3,6 +3,11 @@ variable "rhel_version" {
   default     = "RHEL9"
 }
 
+variable "ticket_number" {
+  description = "SNOW Ticket Number"
+  default     = ""
+}
+
 variable "instance_name_convention" {
   description = "VM instance name convention"
   default     = "web"
